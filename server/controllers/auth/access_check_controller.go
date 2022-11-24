3e19a2166871f4df5c29bb2f51c5d43b44afa81f
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-24 23:51:26 +0900

    Add LoginCheck

--- server/controllers/auth/access_check_controller.go
+++ server/controllers/auth/access_check_controller.go
@@ -1,1 +1,19 @@
 package auth
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/okoge-kaz/golang-todo-application/server/helpers"
+)
+
+// user login check
+func LoginCheck(ctx *gin.Context) {
+	switch helpers.GetUserID(ctx) {
+	case 0: // not login
+		ctx.Redirect(http.StatusFound, "http://localhost:3000/login") // フロントエンド側にリダイレクトさせる
+		ctx.Abort()
+	default: // login
+		ctx.Next()
+	}
+}
