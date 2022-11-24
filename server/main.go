a194d6a7bc28a3fb5bc96d9b7cc2b9e096195e69
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-24 14:59:51 +0900

    main

--- server/main.go
+++ server/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
+	"fmt"
+	"os"
+
+	"github.com/okoge-kaz/golang-todo-application/server/db"
+	"github.com/okoge-kaz/golang-todo-application/server/router"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-	log.Fatal(router.Run(":8080"))
+	// initialize database connection
+	db.Init(
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	// initialize router
+	router := router.Init()
+
+	// start server
+	const port = 8000
+	router.Run(fmt.Sprintf(":%d", port))
 }
