3a2b7bbeec1ae5cde9a6ff1b97632f6186acead7
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-25 11:00:53 +0900

    Add user dynamic pages

---
+++ web/pages/user/[id]/chnage_name.tsx
@@ -0,0 +1,17 @@
+import Header from 'components/layout/Header'
+
+const ChangeName = () => {
+  return (
+    <div className="container mx-auto">
+      <Header />
+
+      <main>
+        <h1 className="text-4xl font-bold text-center mt-10">
+          Change Name
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default ChangeName
---
+++ web/pages/user/[id]/chnage_password.tsx
@@ -0,0 +1,17 @@
+import Header from 'components/layout/Header'
+
+const ChangePassword = () => {
+  return (
+    <div className="container mx-auto">
+      <Header />
+
+      <main>
+        <h1 className="text-4xl font-bold text-center mt-10">
+          Change Password
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default ChangePassword
