151c66ceb5d0f1fbb0bbd6d5dffce28259e02013
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-25 13:27:32 +0900

    update task/[id]

--- web/pages/task/[id]/index.tsx
+++ web/pages/task/[id]/index.tsx
@@ -1,6 +1,13 @@
 import Header from 'components/layout/Header'
+import Task from 'interfaces/task'
+import { GetServerSideProps } from "next"
+import router from 'next/router'
 
-const Task = () => {
+type Props = {
+  task: Task
+}
+
+const Task = (props: Props) => {
   return (
     <div className="container mx-auto">
       <Header />
@@ -9,9 +16,28 @@ const Task = () => {
         <h1 className="text-4xl font-bold text-center mt-10">
           Task
         </h1>
+
+        <div>
+          <h2 className="text-2xl font-bold">{props.task.title}</h2>
+          <p>{props.task.description}</p>
+          <p>{props.task.status}</p>
+        </div>
+
       </main>
     </div>
   )
 }
 
 export default Task
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const id = router.query.id
+  const res = await fetch(`http://localhost:8000/task/${id}`)
+  const task: Task = await res.json()
+
+  return {
+    props: {
+      task,
+    },
+  }
+}
