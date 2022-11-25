29baa70fee7eee1f77f08d46937b08aaa2dc2c4b
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-25 11:04:31 +0900

    Add category pages

---
+++ web/pages/category/index.tsx
@@ -0,0 +1,17 @@
+import Header from 'components/layout/Header'
+
+const CategoryList = () => {
+  return (
+    <div className="container mx-auto">
+      <Header />
+
+      <main>
+        <h1 className="text-4xl font-bold text-center mt-10">
+          Category List
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default CategoryList
