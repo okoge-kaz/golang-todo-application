8564e473edd7bde7502fc0e44ac9529c8cc6fe88
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-25 11:00:40 +0900

    Add user pages

---
+++ web/pages/user/info.tsx
@@ -0,0 +1,17 @@
+import Header from 'components/layout/Header'
+
+const UserInformation = () => {
+  return (
+    <div className="container mx-auto">
+      <Header />
+
+      <main>
+        <h1 className="text-4xl font-bold text-center mt-10">
+          User Information
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default UserInformation
