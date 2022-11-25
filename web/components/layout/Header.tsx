b2c598a20038c5c8d961a4b338c8572540fd58ad
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-25 10:19:37 +0900

    Add Layouts

---
+++ web/components/layout/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header>
+      <div className='bg-sky-500 flex'>
+        <div className='flex-auto'>
+          Sign Up
+        </div>
+        <div className='flex-auto'>
+          Sign In
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
