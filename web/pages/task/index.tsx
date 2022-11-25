c9839b0ce89cdce9d8a47ce3a292198c5281710d
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-25 11:00:14 +0900

    Add task pages

---
+++ web/pages/task/index.tsx
@@ -0,0 +1,17 @@
+import Header from 'components/layout/Header'
+
+const TaskList = () => {
+  return (
+    <div className="container mx-auto">
+      <Header />
+
+      <main>
+        <h1 className="text-4xl font-bold text-center mt-10">
+          Task List
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default TaskList
---
+++ web/pages/task/new.tsx
@@ -0,0 +1,17 @@
+import Header from 'components/layout/Header'
+
+const TaskNew = () => {
+  return (
+    <div className="container mx-auto">
+      <Header />
+
+      <main>
+        <h1 className="text-4xl font-bold text-center mt-10">
+          New Task
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default TaskNew
