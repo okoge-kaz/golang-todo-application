87da568baeb1cb41ff065e349d1ead2c81f724d8
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-25 11:48:59 +0900

    Add interfaces

---
+++ web/interfaces/task.ts
@@ -0,0 +1,11 @@
+export interface Task {
+  id: number
+  title: string
+  deadline: Date
+  status: string
+  description: string
+  is_done: boolean
+  category: string
+  created_at: Date
+  updated_at: Date
+}
---
+++ web/interfaces/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+  id: number
+  name: string
+  created_at: Date
+  updated_at: Date
+}
